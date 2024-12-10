e79b83979198b0aee7d2534aefc5194a9632f4ab
saizen408 <llkoole7@gmail.com>
2024-12-10 08:50:49 -0500

    pid: initial layout of pid logic created

--- kondor-os/sys_control/src/main.c
+++ kondor-os/sys_control/src/main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
@@ -35,6 +36,7 @@
 #define PWM_DEV_NODE DT_ALIAS(pwmfan0)
 #define DEFAULT_PERIOD_CYCLE 88
 #define DEFAULT_PULSE_CYCLE 44
+#define SOC_TARGET_TEMP 35
 #define DEFAULT_PWM_PORT 0
 
 /* Shared BRAM*/
@@ -44,6 +46,70 @@ static const struct gpio_dt_spec leds[] = {GPIO_DT_SPEC_GET(LED0_NODE, gpios),
 					   GPIO_DT_SPEC_GET(LED1_NODE, gpios)};
 static const struct device *const hw_wdt_dev = DEVICE_DT_GET_OR_NULL(WDT_NODE);
 static const struct device *const pwm_dev = DEVICE_DT_GET_OR_NULL(PWM_DEV_NODE);
+
+/*PID structures*/
+typedef struct {
+	float Dt;
+	float Max;
+	float Min;
+	float Kp;
+	float Kd;
+	float Ki;
+	float prevError;
+	float Integral;
+	float currDuty;
+} FixedPid;
+
+static FixedPid pid;
+
+void pidInit(FixedPid *pid) {
+	pid->Dt = 0.1;
+	pid->Max = 100;
+	pid->Min = 5;
+	pid->Kp = 0.1;
+	pid->Kd = 0.01;
+	pid->Ki = 0.5;
+	pid->currDuty = 50;
+
+}
+
+static uint32_t calcTempAdjust(FixedPid *pid, float currTemp){
+	// calculate error
+	float tempError = SOC_TARGET_TEMP - currTemp;
+
+	// proportional term
+	float pout = pid->Kp * tempError;
+
+	// integral term
+	float iout = pid->Ki * pid->Integral;
+
+	// derivative term
+	float derivative = (tempError - pid->prevError) / pid->Dt;
+	float dout = pid->Kd * derivative;
+
+	// calculate total output
+	float output = pout + iout + dout;
+	printf("Actual output: %.3f\n", (double)output);
+
+	// Restrict to max/min
+	if(pid->currDuty >= pid->Max){
+		output = pid->Max;
+		pid->currDuty = pid->Max;
+	} else if(pid->currDuty < pid->Min){
+		output = pid->Min;
+		pid->currDuty = pid->Min;
+	} else {
+		pid->currDuty += (output * -1);
+	}
+
+
+	// save error to previous error
+	pid->prevError = tempError;
+	printf("Current temp is: %.3f | Adujust duty cycle to: %.3f \n", (double)currTemp, (double)pid->currDuty);
+
+	return (uint32_t)((pid->currDuty/(float)100.0)*(float)DEFAULT_PERIOD_CYCLE);
+
+}
 
 static void task_wdt_callback(int channel_id, void *user_data)
 {
@@ -130,6 +196,11 @@ int main(void)
 		printk("PWM device is not ready\n");
 		return STATUS_ERROR;
 	}
+
+	//initialize pid for pwm control
+	pidInit(&pid);
+	
+	// initialize fan to max speed
 	if(pwm_set_cycles(pwm_dev, DEFAULT_PWM_PORT, 
 		DEFAULT_PERIOD_CYCLE, DEFAULT_PULSE_CYCLE, 0)){
 		printk("Failed to set period and pulse width\n");
@@ -152,10 +223,23 @@ int main(void)
 		if(led_state && beat_count >= 4){
 			sleep_time = 1000;
 			beat_count = 0;
+			
+			// check contents of shared bram (temp value provided by versal syscon)
+			// uint32_t val = sys_read32((mem_addr_t)(uintptr_t)SHARED_BRAM_BASE_OFFSET);
+			// printk("0xA8090000 val: %x\n", val);
+			// invoke pid routine to update pwm
+			uint32_t newDuty = calcTempAdjust(&pid, SOC_TARGET_TEMP + 1);
+			printk("setting pwm to: %u\n", newDuty);
+
+			if(pwm_set_cycles(pwm_dev, DEFAULT_PWM_PORT, 
+				DEFAULT_PERIOD_CYCLE, newDuty, 0)){
+				printk("Failed to set period and pulse width of %u\n", newDuty);
+				return STATUS_ERROR;
+			}
 		} else {
 			//'thump-thump' state
 			sleep_time = 100;
-			sys_write32((uint32_t)beat_count, (mem_addr_t)(uintptr_t)SHARED_BRAM_BASE_OFFSET);
+			// sys_write32((uint32_t)beat_count, (mem_addr_t)(uintptr_t)SHARED_BRAM_BASE_OFFSET);
 			// printf("thump..\n");
 		}
 		ret = task_wdt_feed(task_wdt_id);
@@ -163,9 +247,6 @@ int main(void)
 			printk("Failed to feed watch dog: %d\n", ret);
 		}
 		k_msleep(sleep_time);
-		//check contents of shared bram
-		uint32_t val = sys_read32((mem_addr_t)(uintptr_t)SHARED_BRAM_BASE_OFFSET);
-		printk("0xA8090000 val: %x\n", val);
 	}
 	/* shouldn't be here*/
 	return STATUS_ERROR;
